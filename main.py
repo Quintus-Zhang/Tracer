44fc504faa76ee845d31a6f0ba9c7c8f2241d5e9
Quintus <quancheng.zhang@rutgers.edu>
2018-04-23 16:55:08 -0400

    mp for whole thing
    Former-commit-id: 1b043e7b2643b3a8d43a42bdc016ddf290944e27

--- main.py
+++ main.py
@@ -6,7 +6,40 @@ from functions import *
 from dp import dp_solver
 from cal_ce import cal_certainty_equi, generate_consumption_process
 from constants import *
-import matplotlib.pyplot as plt
+import multiprocessing as mp
+import itertools
+
+
+def run_model(TERM, rho, gamma):
+    start = time.time()
+    print(f'########## Term: {TERM} | Rho: {rho:.2f} | Gamma: {gamma} ##########')
+
+    # get conditional survival probabilities
+    cond_prob = surv_prob.loc[START_AGE:END_AGE - 1, 'CSP']  # 22:99
+    cond_prob = cond_prob.values
+
+    ###########################################################################
+    #                  DP - generate consumption functions                    #
+    ###########################################################################
+    c_func_fp = os.path.join(base_path, 'results', f'c function_{TERM}_{rho:.2f}_{gamma}.xlsx')
+    c_func_df, _ = dp_solver(income_bf_ret, income_ret, sigma_perm, sigma_tran, cond_prob, TERM, rho, gamma)
+    c_func_df.to_excel(c_func_fp)
+
+    ###########################################################################
+    #        CE - calculate consumption process & certainty equivalent        #
+    ###########################################################################
+    c_ce_arr = np.zeros(N)
+    for i in range(N):
+        c_proc, _ = generate_consumption_process(income_bf_ret, sigma_perm, sigma_tran, c_func_df, TERM, rho)
+
+        prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
+
+        c_ce, _ = cal_certainty_equi(prob, c_proc, gamma)
+        c_ce_arr[i] = c_ce
+
+    print(f"------ {time.time() - start} seconds ------")
+    return c_ce_arr.mean()
+
 
 start_time = time.time()
 
@@ -38,9 +71,7 @@ income_ret = income_bf_ret[-1]
 sigma_perm = std.loc['sigma_permanent', 'Labor Income Only'][education_level[AltDeg]]
 sigma_tran = std.loc['sigma_transitory', 'Labor Income Only'][education_level[AltDeg]]
 
-# get conditional survival probabilities
-cond_prob = surv_prob.loc[START_AGE:END_AGE - 1, 'CSP']  # 22:99
-cond_prob = cond_prob.values
+
 
 # read isa params
 isa_params = pd.read_excel(isa_fp)
@@ -49,57 +80,19 @@ ce = isa_params[["TERM FOR ISA", "1- rho"]].copy()
 ce = pd.concat([ce]*7, ignore_index=True)
 ce['gamma'] = np.repeat([0, 0.5, 1, 2, 3, 3.5, 4], 15)
 
-for j in range(len(ce)):
-    TERM = ce.loc[j, 'TERM FOR ISA']
-    rho = ce.loc[j, '1- rho']
-    gamma = ce.loc[j, 'gamma']
+search_args = list(itertools.product(ce.values[:, 0], ce.values[:, 1], ce.values[:, 2]))
 
-    start = time.time()
-    print(f'########## Term: {TERM} | Rho: {rho:.2f} | Gamma: {gamma} ##########')
-    ###########################################################################
-    #                  DP - generate consumption functions                    #
-    ###########################################################################
-    if run_dp:
-        c_func_fp = os.path.join(base_path, 'results', f'c function_{TERM}_{rho:.2f}_{gamma}.xlsx')
-        # v_func_fp = os.path.join(base_path, 'results', 'v function_' + education_level[AltDeg] + '.xlsx')
-        c_func_df, v = dp_solver(income_bf_ret, income_ret, sigma_perm, sigma_tran, cond_prob, TERM, rho, gamma)
-        c_func_df.to_excel(c_func_fp)
-        # v.to_excel(v_func_fp)
-    else:
-        c_func_fp = os.path.join(base_path, 'results', 'Iteration_8.xlsx')
-        c_func_df = pd.read_excel(c_func_fp)
+with mp.Pool(processes=mp.cpu_count()) as p:
+    c_ce = p.starmap(run_model, search_args)
 
-    ###########################################################################
-    #        CE - calculate consumption process & certainty equivalent        #
-    ###########################################################################
-    c_proc_fp = os.path.join(base_path, 'results', 'c process_' + education_level[AltDeg] + '.xlsx')
-    inc_proc_fp = os.path.join(base_path, 'results', 'inc process_' + education_level[AltDeg] + '.xlsx')
-
-    c_ce_arr = np.zeros(N)
-    for i in range(N):
-        c_proc, inc = generate_consumption_process(income_bf_ret, sigma_perm, sigma_tran, c_func_df, TERM, rho)
-
-        # c_proc = pd.DataFrame(c_proc)
-        # c_proc.to_excel(c_proc_fp, index=False)
-
-        # inc = pd.DataFrame(inc)
-        # inc.to_excel(inc_proc_fp, index=False)
-
-        prob = surv_prob.loc[START_AGE:END_AGE, 'CSP'].cumprod().values
-
-        c_ce, _ = cal_certainty_equi(prob, c_proc, gamma)
-        c_ce_arr[i] = c_ce
-
-    ce.loc[j, 'Consumption CE'] = c_ce_arr.mean()
-
-    print(f"------ {time.time() - start} seconds ------")
+ce['Consumption CE'] = c_ce
 
 ce.to_excel(ce_fp)
 
 
 # Params check
-print('STD:', c_ce_arr.std())
-print('Mean:', c_ce_arr.mean())
+# print('STD:', c_ce_arr.std())
+# print('Mean:', c_ce_arr.mean())
 print("--- %s seconds ---" % (time.time() - start_time))
 print('AltDeg: ', AltDeg)
 print('permanent shock: ', sigma_perm)
@@ -108,7 +101,5 @@ print('lambda: ', ret_frac[AltDeg])
 print('theta: ',  unemp_frac[AltDeg])
 print('pi: ', unempl_rate[AltDeg])
 print('W0: ', INIT_WEALTH)
-print('Gamma: ', gamma)
-print('rho: ', rho)
-print('term: ', TERM)
+
 
